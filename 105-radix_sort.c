4612b7474fb21e26c899dcc25d805164594ca497
kiddingmz <bata.isidro@gmail.com>
2023-09-26 03:32:34 +0200

    CHORE: 9. Radix sort

---
+++ 105-radix_sort.c
@@ -0,0 +1,85 @@
+#include "sort.h"
+
+/**
+ * get_max - get the maximum value in an array
+ *
+ * @array: an array of integers
+ * @size: the size of the array
+ *
+ * Return: the maximum integer
+ */
+
+int get_max(int *array, int size)
+{
+	int max, i;
+
+	for (max = array[0], i = 1; i < size; i++)
+	{
+		if (array[i] > max)
+			max = array[i];
+	}
+
+	return (max);
+}
+
+/**
+ * radix_counting_sort - counting sort algorithm
+ *
+ * @array: an array of integers
+ * @size: The size of the array
+ * @sig: The significant digit to sort on
+ * @buff: A buffer to store the sorted array
+ *
+ * Return: nothing
+ */
+
+void radix_counting_sort(int *array, size_t size, int sig, int *buff)
+{
+	int count[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+	size_t i;
+
+	for (i = 0; i < size; i++)
+		count[(array[i] / sig) % 10] += 1;
+
+	for (i = 0; i < 10; i++)
+		count[i] += count[i - 1];
+
+	for (i = size - 1; (int)i >= 0; i--)
+	{
+		buff[count[(array[i] / sig) % 10] - 1] = array[i];
+		count[(array[i] / sig) % 10] -= 1;
+	}
+
+	for (i = 0; i < size; i++)
+		array[i] = buff[i];
+}
+
+/**
+ * radix_sort - radix sort algorithm
+ *
+ * @array: an array of integers
+ * @size: the size of the array
+ *
+ * Return: nothing
+ */
+
+void radix_sort(int *array, size_t size)
+{
+	int max, sig, *buff;
+
+	if (array == NULL || size < 2)
+		return;
+
+	buff = malloc(sizeof(int) * size);
+	if (buff == NULL)
+		return;
+
+	max = get_max(array, size);
+	for (sig = 1; max / sig > 0; sig *= 10)
+	{
+		radix_counting_sort(array, size, sig, buff);
+		print_array(array, size);
+	}
+
+	free(buff);
+}
